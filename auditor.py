f24f51ca8f225d6eb515cf533e4cbf0d5f24ed72
SkyC0met <simjiaennichole@gmail.com>
2026-09-11 11:41:35 +0800

    tut 2

--- auditor.py
+++ auditor.py
@@ -8,14 +8,13 @@ while inventory < 500:
     print(f'Number of Failed/Rejected Entries: {failed}')
     break
   elif user_input.isdigit():
-    if int(user_input) > 0:
+    if int(user_input) >= 0:
       inventory += int(user_input)
       failed -= 1
     else:
       print('No negative numbers')
   else:
     print('Please enter a number')
-  print('hello')
   failed += 1
 else:
   print('ALERT: Too many units')
