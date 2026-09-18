50158be54832df4fc4d7f2d3099a691b75d1a580
SkyC0met <simjiaennichole@gmail.com>
2026-09-18 11:15:03 +0800

    commit

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,20 +1,42 @@
 inventory = 0
+new_value = 0
 failed = 0
 
+def get_valid_input():
+  global failed
+
+  while True:
+    user_input = input('Enter "Quit" to exit \nEnter stock quantity: ')
+    if user_input.lower() == 'quit':
+      return 'quit'
+
+    failed += 1
+    
+    try:
+      new_value = int(user_input)
+      if new_value >= 0:
+        failed -= 1
+        return new_value
+      else:
+        print('No negative numbers')
+    except ValueError:
+      print('Please enter a number')
+
+def process_delivery(current_total, new_value):
+  current_total += int(new_value)
+  return current_total
+
+def calculate_tax(amount):
+  pass
+
+def generate_report(total_units, failed_attempts):
+  print(f'Total Units Processed: {total_units}')
+  print(f'Number of Failed/Rejected Entries: {failed_attempts}')
+
 while inventory < 500:
-  user_input = input('Enter "Quit" to exit \nEnter stock quantity: ')
-  if user_input.lower() == 'quit':
-    print(f'Total Units Processed: {inventory}')
-    print(f'Number of Failed/Rejected Entries: {failed}')
+  user_input = get_valid_input()
+  if user_input == 'quit':
+    generate_report(inventory, failed)
     break
-  elif user_input.isdigit():
-    if int(user_input) >= 0:
-      inventory += int(user_input)
-      failed -= 1
-    else:
-      print('No negative numbers')
   else:
-    print('Please enter a number')
-  failed += 1
-else:
-  print('ALERT: Too many units')
+    inventory = process_delivery(inventory, user_input)
